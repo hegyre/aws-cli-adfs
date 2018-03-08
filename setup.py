23a7f6ff0be7eae428e6e9c2dc013b4fc4d2fdb7
luktom <luktom@luktom.net>
2018-03-08 16:33:00 +0100

    Add missing requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='aws-cli-adfs',
   description='Provides aws-adfs-login command that authenticates user to AWS using Active Directory Federation Services (with SAML).',
-  version='0.1.2',
+  version='0.1.3',
   url='https://github.com/tomaszkiewicz/aws-cli-adfs',
   author='Lukasz Tomaszkiewicz',
   author_email='pypi@luktom.net',
@@ -12,6 +12,7 @@ setup(
     'beautifulsoup',
     'bs4',
     'lxml',
-    'boto'
+    'boto',
+    'requests'
   ]
 )
